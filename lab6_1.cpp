ed8a2a24ca23e5d031c5a6344ec4c2f501a7e64d
orlyomyim <yinpiao_wongtrakunme@cmu.ac.th>
2022-12-09 16:11:55 +0700

    lab6 เเยกเลข

--- lab6_1.cpp
+++ lab6_1.cpp
@@ -1,9 +1,28 @@
 #include<iostream>
 using namespace std;
 
-int main(){
-    cout << "Enter an integer: ";
-    cout << "#Even numbers = ";
-    cout << "#Odd numbers = ";
+int main()
+{
+    int number = 1 ;
+    int x = 0 ;
+    int y = 0 ;
+    cout << "Enter an integer: " ;
+    cin >> number ;
+    
+    while (number != 0)
+    {
+        if(number%2 == 0){
+        x += 1 ;
+        }
+        else{
+        y +=1 ;    
+        }
+        
+        cout << "Enter an integer: " ;
+        cin >> number ; 
+    }
+    cout << "#Even numbers = " << x <<"\n" ;
+    cout << "#Odd numbers = " << y ;
+    
     return 0;
 }
